f920f71b7c07ba2debb571745dc98efef32ee7c3
Spencer Wolf <swolf@lcs.com>
2021-08-27 17:42:17 -0400

    Split out utility functions?

---
+++ pragmatic_automation.py
@@ -0,0 +1,30 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def getDriver():
+    opts = Options()
+    opts.headless = True
+    opts.add_argument('--ignore-certificate-errors')
+    opts.add_argument('--ignore-ssl-errors')
+    return Chrome(options=opts)
+
+def log_in(driver, username, password):
+    driver.get('https://pragmatik.lcsinternalservices.lcs.dev/Account/Login')
+    print(driver.title)
+    username_field = driver.find_element_by_name("Username")
+    password_field = driver.find_element_by_name("Password")
+    username_field.send_keys(username)
+    password_field.send_keys(password)
+    password_field.send_keys(Keys.RETURN)
+    print(driver.current_url)
+
+def send_dnr_emails(driver):
+    print("Not implemented: sending emails")
+
+
+def set_dnr(driver):
+    print("Not implemented: set dnr")
--- pymatic.py
+++ pymatic.py
@@ -1,24 +1,7 @@
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-opts = Options()
-opts.headless = True
-opts.add_argument('--ignore-certificate-errors')
-opts.add_argument('--ignore-ssl-errors')
-driver = Chrome(options=opts)
+from pragmatic_automation import * 
 
-# logs in
-driver.get('https://pragmatik.lcsinternalservices.lcs.dev/Account/Login')
-print(driver.title)
-username_field = driver.find_element_by_name("Username")
-password_field = driver.find_element_by_name("Password")
-username_field.send_keys("swolf")
-password_field.send_keys("Sw052021!")
-password_field.send_keys(Keys.RETURN)
-print(driver.current_url)
+driver = getDriver()
+log_in(driver, "swolf", "Sw052021!")
 
 driver.get('https://pragmatik.lcsinternalservices.lcs.dev/#/teamstatusreview')
 driver.implicitly_wait(1)
