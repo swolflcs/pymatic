5816e4152e1d8a13d559d3be386ed7ba1f5913ef
Spencer Wolf <swolf@lcs.com>
2021-08-31 14:51:22 -0400

    Remove redundant start message

--- pymatic.py
+++ pymatic.py
@@ -10,7 +10,7 @@ with open('config.json', 'r') as file:
 def start_tasks():
     schedule.every().day.at(config["reminderTime"]).do(send_reminder_task, config).tag('task')
     schedule.every().day.at(config["sendTime"]).do(send_status_task, config).tag('task')
-    log("Starting Pymatic Tasks")
+    log("Starting Pymatic Tasks, leave this terminal running")
 
 def clear_tasks():
     schedule.clear('task')
@@ -21,7 +21,6 @@ schedule.every().saturday.at("00:00").do(clear_tasks)
 
 if datetime.datetime.today().weekday() < 5:
     start_tasks()
-    log("Pymatic has started, leave this terminal running")
 else:
     log("Pymatic will start on Monday, leave this terminal running")
 
